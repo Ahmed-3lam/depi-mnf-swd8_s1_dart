31b2ff2d39af59d025fa5b28ddae569d5fbbaa2e
Ahmed Allam <ahmed.allam@izam.com>
2025-07-07 20:44:40 +0300

    Implement personal details printing functionality and remove unused arithmetic functions

--- main.dart
+++ main.dart
@@ -1,33 +1,13 @@
 import 'dart:io';
 
+import 'my_methods.dart';
+
 void main() {
-  var result = add(
-    2,
-    4,
+  printMyPersonalDetails(
+    40,
+    name: "Ahmed",
+    university: "Suez",
+    college: "Petroleum",
+    age: 12,
   );
-  printResult(add(
-    2,
-    4,
-  ));
-}
-
-//Function with unNamed && required Params 
-void printResult(int result) {
-  print("The Result is $result");
-}
-
-int add(int a, int b) {
-  return a + b;
-}
-
-int sub(int a, int b) {
-  return a - b;
-}
-
-int multiply(int a, int b) {
-  return a * b;
-}
-
-double divide(int a, int b) {
-  return a / b;
 }
