16bb09817c10052cd1559aba02cf22771d2570c0
Ahmed Allam <ahmed.allam@izam.com>
2025-07-14 19:28:34 +0300

    Named Constructor

---
+++ car.dart
@@ -0,0 +1,17 @@
+class Car {
+  int? id;
+  String? name;
+  double? price;
+
+  Car(int id, String name2, double price) {
+    this.id = id;
+    this.name = name2;
+    price = price;
+  }
+
+  Car.fromJson(Map<String, dynamic> jsonMap) {
+    this.id = jsonMap["id"];
+    this.name = jsonMap["name"];
+    this.price = jsonMap["price"];
+  }
+}
--- main.dart
+++ main.dart
@@ -1,13 +1,25 @@
 import 'dart:io';
+import 'car.dart';
 
-import 'my_methods.dart';
-
-void main() {
-  printMyPersonalDetails(
-    40,
-    name: "Ahmed",
-    university: "Suez",
-    college: "Petroleum",
-    age: 12,
-  );
+void main() async {
+  /// DataType variableName = Value ;
+  List<Car> carList = [];
+  Map<String, dynamic> myMap = {};
+  int i = 0;
+  for (var item in jsonList) {
+    Car car = Car.fromJson(item);
+    carList.add(car);
+  }
+  print(carList);
 }
+
+List<Map<String, dynamic>> jsonList = [
+  {"id": 12, "name": "Car 1", "Price": 200.0},
+  {"id": 12, "name": "Car 1", "Price": 200.0},
+  {"id": 12, "name": "Car 1", "Price": 200.0},
+  {"id": 12, "name": "Car 1", "Price": 200.0},
+  {"id": 12, "name": "Car 5", "Price": 200.0},
+  {"id": 12, "name": "Car 6", "Price": 200.0},
+  {"id": 12, "name": "Car 7", "Price": 200.0},
+  {"id": 12, "name": "Car 8", "Price": 200.0},
+];
