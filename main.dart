e17887a43030d9d92cc6d6d616178f530eba2d70
Ahmed Allam <ahmed.allam@izam.com>
2025-07-28 18:23:33 +0300

    enum

--- main.dart
+++ main.dart
@@ -1,8 +1,34 @@
-import 'animal.dart';
-
 void main() async {
-  Lion lion1 = Lion();
-  SpecificLion lion2 = SpecificLion();
-  lion1.eat();
+  String base_url = getBaseUrl(Environment.PROD);
 
+  void fetchData() {
+    var date = base_url + "/date";
+  }
+
+  void fetchData2() {
+    var date = base_url + "/date";
+  }
+
+  void fetchData3() {
+    var date = base_url + "/date";
+  }
+}
+
+enum Environment {
+  PROD,
+  STAGING,
+  DEV,
+}
+
+String getBaseUrl(Environment env) {
+  switch (env) {
+    case Environment.DEV:
+      return "www.tager.dev.com";
+
+    case Environment.STAGING:
+      return "www.tager.staging.com";
+
+    default:
+      return "www.tager.prod.com";
+  }
 }
